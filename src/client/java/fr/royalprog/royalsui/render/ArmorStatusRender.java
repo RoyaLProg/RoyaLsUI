5dd4b4ea5db2d7ff77fffdf9a19ac9b8f89c4728
Camille CAMBIUM <royalfr@protonmail.com>
2023-12-27 06:43:53 +0100

    trying to make a image appear :/

--- src/client/java/fr/royalprog/royalsui/render/ArmorStatusRender.java
+++ src/client/java/fr/royalprog/royalsui/render/ArmorStatusRender.java
@@ -3,12 +3,13 @@ package fr.royalprog.royalsui.render;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
 
 public class ArmorStatusRender {
 
     private final MinecraftClient minecraft;
     private final TextRenderer textRenderer;
-
+    private static final Identifier part = new Identifier("royalsui", "gui/sprites/cancel");
     public ArmorStatusRender() {
         minecraft = MinecraftClient.getInstance();
         textRenderer = minecraft.textRenderer;
@@ -16,6 +17,7 @@ public class ArmorStatusRender {
 
     public void render (DrawContext context, int x, int y)
     {
-        context.drawText(textRenderer, "It is alive ! Or is it ?", x, y, 0xFF0000, false);
+        // context.drawText(textRenderer, "It is alive ! Or is it ?", x, y, 0xFF0000, false);
+        context.drawGuiTexture(part, x, y, 20, 20);
     }
 }
