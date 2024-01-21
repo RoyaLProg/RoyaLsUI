63cb04f282344643690be78e98e417d181c304fa
Camille CAMBIUM <royalfr@protonmail.com>
2024-01-21 04:04:44 +0100

    Logic is done

--- src/client/java/fr/royalprog/royalsui/render/ArmorStatusRender.java
+++ src/client/java/fr/royalprog/royalsui/render/ArmorStatusRender.java
@@ -6,25 +6,45 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+
+import java.awt.*;
+import java.lang.reflect.Array;
 
 public class ArmorStatusRender implements HudRenderCallback {
 
     private final MinecraftClient minecraft;
     private final TextRenderer textRenderer;
+
     private static final Identifier part = new Identifier(RoyaLsUIClient.MOD_ID, "textures/gui/sprites/cancel.png");
     public ArmorStatusRender() {
         minecraft = MinecraftClient.getInstance();
         textRenderer = minecraft.textRenderer;
     }
 
+    public int renderPart(DrawContext drawContext, ItemStack item, Identifier part, int xoffset, int yoffset) {
+        if (item.isEmpty())
+            return 0;
+        Color color = new Color(item.getItemBarColor());
+        RenderSystem.setShaderTexture(0, part);
+        RenderSystem.setShaderColor(color.getRed(), color.getGreen(), color.getBlue(), 1.0F);
+        RenderSystem.setShader(GameRenderer::getRenderTypeTextProgram);
+        drawContext.drawTexture(part, 0 + xoffset, 0 + yoffset, 0, 0, 12, 12, 12, 12);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        return (15);
+    }
+
     @Override
     public void onHudRender(DrawContext drawContext, float tickDelta) {
 //        drawContext.drawGuiTexture(part, 0, 0, 30, 30);
-        RenderSystem.setShaderTexture(0, part);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShader(GameRenderer::getRenderTypeTextProgram);
-        drawContext.drawTexture(part, 0, 0, 0, 0, 12, 12, 12, 12);
+        ClientPlayerEntity player = minecraft.player;
+        Identifier[] parts = {part, part, part, part};
+        int offset = 0;
+
+        for (int i = 0; i < parts.length; i++)
+            offset += renderPart(drawContext, player.getInventory().getArmorStack(i), parts[i], 0, offset);
     }
 }
