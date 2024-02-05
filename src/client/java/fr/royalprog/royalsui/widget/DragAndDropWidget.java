a989a1b706c744c866d83331b21a6316f86b9b5b
CAMBIUM camille <ccambium@gmail.com>
2024-02-05 17:53:59 +0100

    configurability of armor via new widget

--- src/client/java/fr/royalprog/royalsui/widget/DragAndDropWidget.java
+++ src/client/java/fr/royalprog/royalsui/widget/DragAndDropWidget.java
@@ -7,7 +7,6 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.sound.SoundManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -20,30 +19,46 @@ public class DragAndDropWidget extends ClickableWidget {
     private final Color _wcolor;
     private final String _name;
 
+    private int maxX;
+    private int maxY;
+
+    private double valueX;
+    private double valueY;
+
+
     private final int _screenHeight;
     private final int _screenWidth;
 
     private double mouseDeltaX;
     private double mouseDeltaY;
-
     private int value;
 
     private final TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
 
     private static final Identifier TEXTURE = new Identifier(RoyaLsUIClient.MOD_ID, "textures/gui/sprites/border2.png");
     public DragAndDropWidget(int x, int y, int width, int height, String name, Color wcolor, int screenWidth, int screenHeight) {
-        super(x, y, width, height, Text.literal(name));
+        super(MathHelper.clamp(x, 0, screenWidth - width), MathHelper.clamp(y, 0, screenHeight - height), width, height, Text.literal(name));
+
+
         _wcolor = wcolor;
         _name = name;
         _screenHeight = screenHeight;
         _screenWidth = screenWidth;
+        maxX = _screenWidth - width;
+        maxY = _screenHeight - height;
+        valueX = MathHelper.map(x, 0, maxX, 0, 1);
+        valueY = MathHelper.map(y, 0, maxY, 0, 1);
+        setX((int) (valueX * maxX));
+        setY((int) (valueY * maxY));
     }
 
     protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
         RenderSystem.setShaderColor(_wcolor.getRed(), _wcolor.getGreen(), _wcolor.getBlue(), 1.0F);
-        context.drawTexture(TEXTURE, getX(), getY(), 0, 0, width, height, width, height);
+        context.drawTexture(TEXTURE, (int) (valueX * maxX), (int) (valueY * maxY), 0, 0, width, height, width, height);
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-//        context.drawText(textRenderer, _name, getX() + ((width / 2) - textRenderer.getWidth(_name) / 2), getY() + 5, 0xFFFFFF, false);
+        if (this.isHovered())
+            context.drawTooltip(textRenderer, Text.literal(_name).withColor(0xFFFFFF), mouseX, mouseY);
 //        context.fill(RenderLayer.getGui(), getX(), getY(), width, height, _wcolor.getRGB()); // why doesn't it work ???
 
         /*
@@ -52,8 +67,7 @@ public class DragAndDropWidget extends ClickableWidget {
 
         Not working ? maybe because of tryDraw ?
          */
-        context.drawBorder(0, 0, 100, 100, 0xFFFFFF);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        context.fill(0, 0, 500, 500, -50,0xFFFFFF);
     }
 
     @Override
@@ -62,13 +76,11 @@ public class DragAndDropWidget extends ClickableWidget {
     }
 
     private void setValueFromMouse(double mouseX, double mouseY) {
-        setX((int)MathHelper.clamp(mouseX - mouseDeltaX, 0, _screenWidth - width));
-        setY((int) MathHelper.clamp(mouseY - mouseDeltaY, 0, _screenHeight - height));
+        setX((int)MathHelper.clamp(mouseX - mouseDeltaX, 0, maxX));
+        setY((int)MathHelper.clamp(mouseY - mouseDeltaY, 0, maxY));
+        valueX = MathHelper.map(getX(), 0, maxX, 0, 1);
+        valueY = MathHelper.map(getY(), 0, maxY, 0, 1);
     }
-
-//    private void setValue(double mouseX, mouse) {
-//        setX();
-//    }
 
     protected void onDrag(double mouseX, double mouseY, double deltaX, double deltaY) {
         this.setValueFromMouse(mouseX, mouseY);
